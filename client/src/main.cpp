235da08f116d35a9880780c49b5096f70b69c812
VadimS98 <vadim.sukhomud@gmail.com>
2023-09-16 01:23:02 +0200

    Working Project

---
+++ client/src/main.cpp
@@ -0,0 +1,112 @@
+#include <myproto/error.pb.h>
+#include <myproto/print.grpc.pb.h>
+#include "google/protobuf/empty.pb.h"
+
+#include <grpc/grpc.h>
+#include <grpcpp/create_channel.h>
+
+#include <iostream>
+#include <string>
+
+class MyServiceClient {
+public:
+    MyServiceClient(std::shared_ptr<grpc::Channel> printChannel)
+        : _printStub(greetandprint::Print::NewStub(printChannel)) {}
+
+    void SendCommand(const std::string& command, const std::string& message) \
+    {
+        if (command == "hello") {
+            SendGreeting(command);
+        }
+        else if (command == "print")
+        {
+            SendPrinting(command, message);
+        }
+        else
+        {
+            UnknownCommand(command);
+        }
+    }
+
+private:
+    void UnknownCommand(const std::string& command) 
+    {
+        greetandprint::Command request;
+        request.set_command(command);
+
+        greetandprint::Error response = SendRequest(request);
+
+        std::cout << response.answer() << std::endl;
+    }
+
+    void SendGreeting(const std::string& command)
+    {
+        greetandprint::Command request;
+        request.set_command(command);
+
+        SendRequest(request);
+    }
+
+    void SendPrinting(const std::string& command, const std::string& message)
+    {
+        greetandprint::Command request;
+        request.set_command(command);
+        request.set_text(message);
+
+        SendRequest(request);
+    }
+
+    greetandprint::Error SendRequest(greetandprint::Command request)
+    {
+        greetandprint::Error response;
+        grpc::ClientContext context;
+
+        grpc::Status status = _printStub->Print(&context, request, &response);
+
+        if (status.ok())
+        {
+            std::cout << "Request sent successfully." << std::endl;
+        }
+        else
+        {
+            std::cerr << "RPC failed: " << status.error_message() << std::endl;
+        }
+
+        return response;
+    }
+
+    std::unique_ptr<greetandprint::Print::Stub> _printStub;
+};
+
+int main(int argc, char* argv[])
+{   
+    // Initialize ClientObj && connect to server via server address
+    std::string server_address("localhost:50051");
+    MyServiceClient printClient(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
+
+    // Pick && parse command from command line to form msg
+
+    std::string input = "";
+    while (1) {
+        getline(std::cin, input);
+        std::string firstWord = input.substr(0, input.find(" "));
+        
+        std::string message = "";
+
+        if (firstWord == "print") {
+            message = input.substr(input.find(" ") + 1);
+        }
+        else if (firstWord == "exit")
+        {
+            return 0;
+        }
+        else
+        {
+            // Do nothing
+        }
+
+        printClient.SendCommand(firstWord, message);
+    }
+
+    return 0;
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,45 @@
+#include <myproto/error.pb.h>
+#include <myproto/print.grpc.pb.h>
+#include "google/protobuf/empty.pb.h"
+
+#include <grpc/grpc.h>
+#include <grpcpp/server_builder.h>
+
+#include <iostream>
+
+class PrintService final : public greetandprint::Print::Service {
+public:
+    virtual ::grpc::Status Print(::grpc::ServerContext* context, const ::greetandprint::Command* request, ::greetandprint::Error* response)
+    {
+        if (request->command() == "hello") {
+            std::cout << "hello" << std::endl;
+        }
+        else if (request->command() == "print") {
+            std::cout << request->text() << std::endl;
+        }
+        else {
+            std::cout << "Server: GotCommand for \"" << request->command() << "\"." << std::endl;
+
+            response->set_answer("Sorry, currently I'm just a young server who cant handle such requests. Come back later!");
+        }
+
+        return grpc::Status::OK;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    std::string server_address("0.0.0.0:50051");
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+    PrintService printService;
+    builder.RegisterService(&printService);
+
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+    
+    return 0;
+}
